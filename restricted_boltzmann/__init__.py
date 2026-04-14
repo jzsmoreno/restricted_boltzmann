1f0b7ea1f0fda9c6d5d2cb9dc8493940ca32e2e9
jzsmoreno <42299052+jzsmoreno@users.noreply.github.com>
2026-04-14 13:59:19 -0600

    Update __init__.py

--- restricted_boltzmann/__init__.py
+++ restricted_boltzmann/__init__.py
@@ -1,7 +1,7 @@
 """
 Authors: J. A. Moreno-Guerra
 Last modification: 09/06/2024
-Corresponding author: jzs.gm27@gmail.com 
+Corresponding author: jzs.gm27@gmail.com
 """
 
 from restricted_boltzmann.main import *
